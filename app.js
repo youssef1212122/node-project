65ec041a761d0a0a4d93bcc94c4e58352430a733
Youssef_yo8 <youssef.1420065@stemassiut.moe.edu.eg>
2025-09-02 01:56:37 +0300

    update code

--- app.js
+++ app.js
@@ -73,5 +73,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
